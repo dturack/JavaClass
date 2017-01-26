53092150a3e99d3a41fce50f3329c47336f51783
Daniel Turack <dturack@csub.edu>
2017-01-26 15:25:21 -0800

    Add files via upload

---
+++ DTura01/src/member/MemberConst.java
@@ -0,0 +1,33 @@
+package member;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author student
+ */
+public interface MemberConst {
+    
+	String menu = 
+                "/t================ CS 394 Assignment 1 ===============/n"
+		"/tG/g:   Ask for a N, and generate N members of mixed/n"
+                "/t       Member class's objects, and store in a Vector /n"
+		"/t       and a array Objects./n/n"
+		"/tS/s/ : Sort the members in the vector and array in/n"
+                "/t       ascending order./n/n"
+		"/tV/v/ : Show the members in the vector and array ./n/n"
+		"/tO/o/ : Save objects inside vector into a HTML file/n"
+		"/t       with objects saved in the format of HTML/n"
+		"/t       Table./n"
+		"/tF/f  : Show HTML file contents on screen./n/n"
+		"/tL/l  : Launch the default internet browser to/n"
+		"/t       display the generated HTML file./n"
+		"/t--------------------------------------------------/n"
+		"/tH/h/?: Display this menu./n"
+		"/tE/e  : Exit/n"
+		"/t=================================================/n"
+}
