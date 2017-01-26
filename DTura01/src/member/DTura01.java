8d9b2455233fddeae723bf1d4a9bfe29a6fdb3a9
Daniel Turack <dturack@csub.edu>
2017-01-26 15:26:51 -0800

    Add files via upload

--- DTura01/src/member/DTura01.java
+++ DTura01/src/member/DTura01.java
@@ -16,7 +16,7 @@ public class DTura01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.printf("%s/n", Member.Const.menu)
+        System.out.printf("%s/n", Member.Const.menu);
     }
     
 }
--- DTura01/src/member/Member.java
+++ DTura01/src/member/Member.java
@@ -4,11 +4,32 @@
  * and open the template in the editor.
  */
 package member;
-
 /**
  *
  * @author student
  */
-public class Member {
-    
+public class Member implements Comparable {
+    public Member () {        
+    }
+    public Member (Member m) {
+        firstName = m.firstName;
+        firstName = new String(m.firstName);
+    }
+    void generate() {
+        ID = rdn.nextInt(999999999 - 100000000) + 100000000;//randomly generate ID number between 100000000 and 999999999
+        firstName = Names.firstname[rdn.nextInt(Names.firstname.length)]; 
+        lastname = Names.lastname[rdn/nextInt(Names.lastname.length)];
+    }
+	public String toString(
+                String.format("%03d-%02d-%034d %15s %-15s", ID%1000000, ID%10000);
+        );
+	public String toString( boolean lab ){
+            return lab ? "MEM " + toString() : toString();
+        };
+}
+	public int    compareTo(Member m) {return ID - m.ID; };
+	public String htmlRow();
+	public String htmlColumns();
+	protected String firstName, lastName; 
+        int ID;
 }
